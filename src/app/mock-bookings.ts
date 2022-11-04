7f75091e6d173d9914b288c5a31867d5fd889f68
Kerstin Ohlmann <kerstin.ohlmann@lhind.dlh.de>
2022-11-04 10:32:29 +0100

    Add first components, style with bootstrap and mock-data

---
+++ src/app/mock-bookings.ts
@@ -0,0 +1,39 @@
+import { Booking } from "./booking";
+
+export const Bookings: Booking[] = [
+    {
+        id: 1,
+        name: "Kerstin Ohlmann",
+        roomNumber: 100,
+        startDate: new Date(),
+        endDate: new Date("2022-12-31"),
+    },
+    {
+        id: 2,
+        name: "Thorin Eichenschild",
+        roomNumber: 50,
+        startDate: new Date(),
+        endDate: new Date("2022-12-31"),
+    },
+    {
+        id: 3,
+        name: "Sam Winchester",
+        roomNumber: 12,
+        startDate: new Date(),
+        endDate: new Date("2022-12-31"),
+    },
+    {
+        id: 4,
+        name: "Luzifer",
+        roomNumber: 66,
+        startDate: new Date(),
+        endDate: new Date("2022-12-31"),
+    },
+    {
+        id: 5,
+        name: "Bongo MC Doogle",
+        roomNumber: 38,
+        startDate: new Date(),
+        endDate: new Date("2022-12-31"),
+    }
+]
